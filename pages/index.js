e8409a498dbf1de3e7dae0b5dd5c7ad509130e9c
jithingp <jithingpanoli18@gmail.com>
2022-05-23 12:32:25 +0530

    Home page v1

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,9 @@ export default function Home({propertyForSale,propertyForRent}) {
       desc1="Explore Apartments, Villas, Homes" desc2="and more" buttonText="Explore Buying"
       linkname="/search?purpose=for-sale"
       imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"/>
+      <Flex flexWrap='wrap'>
        {propertyForSale.map((property)=><Property property={property} key={property.id}/>)}
+       </Flex>
     </Box>
   )
 }
