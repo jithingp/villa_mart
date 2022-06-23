b88ad8f00704f17f0bb5ef7d7623e2836f6d3fb5
jithingp <jithingpanoli18@gmail.com>
2022-06-23 23:15:55 +0530

    Navbar modified

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,14 +3,27 @@ import { Menu,MenuButton,MenuList,MenuItem,IconButton,Flex,Box,Spacer } from '@c
 import {FcMenu,FcHome,FcAbout} from 'react-icons/fc'
 import {BsSearch} from 'react-icons/bs'
 import {FiKey} from 'react-icons/fi'
+import { useMediaQuery } from '@chakra-ui/react'
 
-const Navbar = ()=>(
-    <Flex p='2' borderBottom='1px' borderColor='gray.500' >
-        <Box fontSize='3xl' color='green.400' fontWeight='bold'>
-            <Link href='/' paddingLeft='2'>Villa_Market</Link>
+const Desk =()=>(
+    <>
+        <Box fontSize='2xl' color='green.400' fontWeight='normal' marginRight='8'>
+            <Link href='/' paddingLeft='2'>Home</Link>
         </Box>
-        <Spacer/>
-        <Box>
+        <Box fontSize='2xl' color='green.400' fontWeight='normal' marginRight='8'>
+            <Link href='/search' paddingLeft='2'>Search</Link>
+        </Box>
+        <Box fontSize='2xl' color='green.400' fontWeight='normal' marginRight='8'>
+            <Link href='/search?purpose=for-sale' paddingLeft='2'>Buy Property</Link>
+        </Box>
+        <Box fontSize='2xl' color='green.400' fontWeight='normal'>
+            <Link href='/search?purpose=for-rent' paddingLeft='2'>Rent Property</Link>
+        </Box>
+    </>
+)
+
+const Mobile=()=>(
+    <Box>
             <Menu>
                 <MenuButton as={IconButton} icon={<FcMenu/>} variant='outlined' color='red.400'/>
                 <MenuList>
@@ -29,8 +42,21 @@ const Navbar = ()=>(
                 </MenuList>
             </Menu>
         </Box>
+)
+
+const Navbar = ()=>{
+    const [isLargerThan500] = useMediaQuery('(min-width: 750px)')
+    
+    return (
+    <Flex p='2' borderBottom='1px' borderColor='gray.500' >
+        <Box fontSize='3xl' color='green.400' fontWeight='bold'>
+            <Link href='/' paddingLeft='2'>Villa_Market</Link>
+        </Box>
+        <Spacer/>
+        {isLargerThan500?<Desk/>:<Mobile/>}
 
     </Flex>
 )
+            }
 
 export default Navbar
